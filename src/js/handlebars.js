dc96d3aac676f3b01ae963eb05274577363de73a
Claudia Friedrich <claudia.friedrich@nsgroup.ch>
2017-06-20 00:14:10 +0200

    add formatted date

--- src/js/handlebars.js
+++ src/js/handlebars.js
@@ -30,6 +30,28 @@ Handlebars.registerHelper('ratingCounter', function(){
     );
 });
 
+Handlebars.registerHelper('FormatedDate', function(date, language){
+    moment.locale(language);
+    let momentDate = moment(date);
+    let today = moment().set({hour: 0, minute: 0, second: 0, millisecond: 0});
+    let days = moment(date, "LLLL");
+    let calendarDate = moment(date).calendar();
+    let dateDiff = momentDate.diff(today, 'days');
+    if(dateDiff>0){
+        return new Handlebars.SafeString('<span> Erledigen bis am <br>' + calendarDate + ' </span>');
+    } else if (dateDiff == 0){
+        return 'Heute erledigen';
+    } else{
+        return new Handlebars.SafeString('<span class="outdated"> Überfällig seit <br>' + calendarDate + ' </span>');
+    }
+});
+
+Handlebars.registerHelper('FormatedDateSimple', function(date, language){
+    moment.locale(language);
+    let formatedDate = moment(date).format('L');
+    return formatedDate;
+});
+
 //Handebar-Render-Function
 function renderPage(){
     let notesData = parseNoteData();
